3d7cef8a70c920a5f8907006fe13a2708b6a7de6
gopika <gopikakallinkal@gmail.com>
2021-02-05 12:19:11 +0530

    e-shop django project

---
+++ Eshop/store/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models.product import Product
+from .models.category import Category
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
---
+++ Eshop/store/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models.product import Product
+from .models.category import Category
+# Create your views here.
+def index(request):
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products();
+    data ={}
+    data['products'] = products
+    data['categories'] = categories
+
+    return render(request, 'index.html', data)
+
+
